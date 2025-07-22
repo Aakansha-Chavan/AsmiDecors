7e428858f994ea8d05a67bcece5cb57f89e30ed2
Aakansha-Chavan <work.aakanshachavan9@gmail.com>
2025-07-23 01:05:36 +0530

    Made changes in navbar for mobile responsiveness

--- public/js/index1.js
+++ public/js/index1.js
@@ -52,9 +52,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
-document.addEventListener("contextmenu", e => e.preventDefault());
-document.addEventListener("keydown", e => {
-  if (e.key === "F12" || (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key))) {
-    e.preventDefault();
-  }
-});
